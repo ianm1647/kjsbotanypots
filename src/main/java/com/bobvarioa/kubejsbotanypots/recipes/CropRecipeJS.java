b604e1a2c5df179f863f8292c5c052777245447e
Bob Varioa <true.email001@gmail.com>
2023-08-17 11:41:54 -0500

    Bump version number, add withRolls

--- src/main/java/com/bobvarioa/kubejsbotanypots/recipes/CropRecipeJS.java
+++ src/main/java/com/bobvarioa/kubejsbotanypots/recipes/CropRecipeJS.java
@@ -1,12 +1,17 @@
 package com.bobvarioa.kubejsbotanypots.recipes;
 
 import com.bobvarioa.kubejsbotanypots.components.DisplayComponent;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import dev.latvian.mods.kubejs.item.InputItem;
 import dev.latvian.mods.kubejs.item.OutputItem;
+import dev.latvian.mods.kubejs.recipe.RecipeJS;
 import dev.latvian.mods.kubejs.recipe.RecipeKey;
 import dev.latvian.mods.kubejs.recipe.component.*;
 import dev.latvian.mods.kubejs.recipe.schema.RecipeSchema;
+import dev.latvian.mods.kubejs.registry.KubeJSRegistries;
 import net.darkhax.botanypots.data.displaystate.DisplayState;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
@@ -16,29 +21,36 @@ public interface CropRecipeJS {
     RecipeKey<String[]> CATEGORIES = StringComponent.ANY.asArray().key("categories");
     RecipeKey<Long> GROWTH_TICKS = TimeComponent.TICKS.key("growthTicks");
     RecipeKey<Float> GROWTH_MODIFIER = NumberComponent.FLOAT.key("growthModifier").optional(1f);
-    RecipeKey<DisplayState> DISPLAY = DisplayComponent.ANY.key("display");
+    RecipeKey<DisplayState[]> DISPLAY = DisplayComponent.ANY.asArrayOrSelf().key("display");
 
-    class LootEntry {
-        float chance;
-        OutputItem output;
-        int minRolls;
-        int maxRolls;
 
-        public LootEntry(float chance, OutputItem output, int minRolls, int maxRolls) {
-            this.chance = chance;
-            this.output = output;
-            this.minRolls = minRolls;
-            this.maxRolls = maxRolls;
+    RecipeComponent<OutputItem> ID_CHANCE = new RecipeComponentWithParent<OutputItem>() {
+
+        @Override
+        public RecipeComponent<OutputItem> parentComponent() {
+            return ItemComponents.OUTPUT;
         }
-    }
 
-    RecipeComponentBuilder LOOT_ENTRY = new RecipeComponentBuilder(4)
-            .add(ItemComponents.OUTPUT.key("output"))
-            .add(NumberComponent.FLOAT.key("chance"))
-            .add(NumberComponent.INT.key("minRolls"))
-            .add(NumberComponent.INT.key("maxRolls"));
+        @Override
+        public @Nullable JsonElement write(RecipeJS recipe, OutputItem value) {
+            var json = RecipeComponentWithParent.super.write(recipe, value).getAsJsonObject();
+            // ngl even after looking at the json many times idk why this is right, i just know it is
+            json.add("output", json.get("item"));
+            return json;
+        }
 
-    RecipeKey<Map<String, Object>[]> DROPS = LOOT_ENTRY.asArray().key("drops");
+        @Override
+        public OutputItem read(RecipeJS recipe, Object from) {
+            if (from instanceof JsonObject json) {
+                json.add("item", json.get("output").getAsJsonObject().get("item"));
+                return RecipeComponentWithParent.super.read(recipe, json);
+            }
+
+            return RecipeComponentWithParent.super.read(recipe, from);
+        }
+    };
+
+    RecipeKey<OutputItem[]> DROPS = ID_CHANCE.asArray().key("drops");
 
     RecipeSchema SCHEMA = new RecipeSchema(SEED, CATEGORIES, DISPLAY, DROPS, GROWTH_TICKS, GROWTH_MODIFIER);
 }
